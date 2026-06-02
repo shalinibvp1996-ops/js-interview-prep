0d5c821a28cbc527d6dab76f9190c35f32ef5084
Shalini Singh <shalinibvp.1996@gmail.com>
2026-06-02 18:14:31 +0900

    Conversion and comparison clarity

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,3 +31,27 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ********************************  Operations  *************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3  = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true); // Not preffered
+console.log(+""); // Not preffered
+
+//Prefix and Postfix read from MDN javascript:-  
+// Link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
